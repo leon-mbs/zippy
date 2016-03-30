d7f5df21edb6c708ce3918d2487d7606567a47b1
Leon <leon.mbs@gmail.com>
2016-03-30 21:53:01 +0300

    composer

---
+++ core/html/form/textinput.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Zippy\Html\Form;
+
+use \Zippy\Validator\Validator;
+use \Zippy\Interfaces\Validated;
+use \Zippy\Interfaces\AjaxRender;
+
+/**
+ * Компонент  тэга  &lt;input type=&quot;text&quot;&gt;
+ */
+class TextInput extends HtmlFormDataElement implements Validated, AjaxRender
+{
+
+    protected $validators = array();
+
+    /**
+     * Конструктор
+     * @param mixed  ID
+     * @param Значение елеента  или  поле  привязанного объекта
+     */
+    public function __construct($id, $value = null)
+    {
+        parent::__construct($id);
+        $this->setValue($value);
+        $this->setAttribute("name", $this->id);
+    }
+
+    /**
+     * Возвращает  текстовое  значение
+     * @return  string
+     */
+    public function getText()
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * Устанавливает  текстовое  значение
+     * @param  string
+     */
+    public function setText($text)
+    {
+        $this->setValue($text);
+    }
+
+    /**
+     * @see  HtmlComponent
+     */
+    public function RenderImpl()
+    {
+        // $this->checkInForm();
+
+        $this->setResponseData();
+    }
+
+    protected function setResponseData()
+    {
+        $this->setAttribute("value", ($this->getValue()));
+    }
+
+    /**
+     * @see SubmitDataRequest
+     */
+    public function getRequestData()
+    {
+
+        $this->setValue($_REQUEST[$this->id]);
+        foreach ($this->validators as $validator) {
+            $validator->validate($this, $this->getValue());
+        }
+    }
+
+    /**
+     * @see Validated
+     */
+    public function addValidator(Validator $validator)
+    {
+        $this->validator[] = $validator;
+    }
+
+    /**
+     * @see AjaxRender
+     */
+    public function AjaxAnswer()
+    {
+
+        $text = $this->getValue();
+        return "$('#{$this->id}').val('{$text}')";
+    }
+
+}
---
+++ demo/pages/main.php
@@ -0,0 +1,14 @@
+<?php
+
+ namespace Pages;
+
+class Main extends \Zippy\Html\WebPage{
+    public function __construct()
+    {
+            parent::__construct();
+            
+    }             
+}     
+    
+
+
