75320e402664ad99ae84626a60fc3679f16025ed
leon.mbs@gmail.com <leon.mbs@gmail.com>
2021-08-21 20:34:08 +0300

    onchange texinput

--- core/html/form/textinput.php
+++ core/html/form/textinput.php
@@ -2,17 +2,23 @@
 
 namespace Zippy\Html\Form;
 
-
+use \Zippy\WebApplication;
+use \Zippy\Interfaces\ChangeListener;
+use \Zippy\Interfaces\Requestable;
 use \Zippy\Interfaces\AjaxRender;
+use \Zippy\Interfaces\EventReceiver;
+use \Zippy\Event;
 
 /**
  * Компонент  тэга  &lt;input type=&quot;text&quot;&gt;
  */
-class TextInput extends HtmlFormDataElement implements AjaxRender
+class TextInput extends HtmlFormDataElement implements ChangeListener, Requestable, AjaxRender
 {
 
     private $defvalue;
     private $dlist;
+    private $event;
+        
     /**
      * Конструктор
      * @param mixed  ID
@@ -64,6 +70,21 @@ class TextInput extends HtmlFormDataElement implements AjaxRender
              
         }
         
+        if ($this->event != null) {
+            $formid = $this->getFormOwner()->id;
+
+            $url = $this->owner->getURLNode() . '::' . $this->id;
+            $url = substr($url, 2 + strpos($url, 'q='));
+
+            if ($this->event->isajax == false) {
+
+                $this->setAttribute("onblur", "javascript:{if(beforeZippy('{$this->id}') ==false) return false; $('#" . $formid . "_q').attr('value','" . $url . "');$('#" . $formid . "').submit();}");
+            } else {
+                $_BASEURL = WebApplication::$app->getResponse()->getHostUrl();
+                $this->setAttribute("onblur", "if(beforeZippy('{$this->id}') ==false) return false; $('#" . $formid . "_q').attr('value','" . $url . "'); submitForm('{$formid}','{$_BASEURL}/?ajax=true');");
+            }
+        }        
+        
     }
 
     protected function setResponseData() {
@@ -104,5 +125,25 @@ class TextInput extends HtmlFormDataElement implements AjaxRender
         $this->dlist  = $list;
         
     }
+    public function RequestHandle() {
+        $this->OnEvent();
+    }   
+     /**
+     * @see  ChangeListener
+     */
+    public function onChange(EventReceiver $receiver, $handler, $ajax = false) {
+
+        $this->event = new Event($receiver, $handler);
+        $this->event->isajax = $ajax;
+    }
+
+    /**
+     * @see ChangeListener
+     */
+    public function OnEvent() {
+        if ($this->event != null) {
+            $this->event->onEvent($this);
+        }
+    }   
     
 }
